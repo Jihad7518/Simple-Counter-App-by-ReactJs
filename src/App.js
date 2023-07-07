8904aa8e5d5177021ddb326bb96efa2d45882acc
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-07-07 23:13:17 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { useState } from "react";
 import "./App.css";
 
@@ -18,14 +17,15 @@ function App() {
     setCount(0);
   }
 
- return (
+
+  return (
     <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
       <div className="text-[#0398d4] font-medium text-2xl">Increament And Decreament</div>
 
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] flex-row ">
         <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
-
-   </button>
+          -
+        </button>
         <div className="font-bold gap-12 text-5xl">
             {count}
         </div>
@@ -33,7 +33,8 @@ function App() {
           +
         </button>
       </div>
-<button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
+      
+      <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg">
         Reset
       </button>
 
