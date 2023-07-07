10f82153eefbf19240576bed03dc927a9c320d81
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-07-07 23:12:49 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,3 +24,12 @@ function App() {
 
       <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151] flex-row ">
         <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+
+   </button>
+        <div className="font-bold gap-12 text-5xl">
+            {count}
+        </div>
+        <button onClick={increaseHandler} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+        </button>
+      </div>
